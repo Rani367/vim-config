87f98554a94dc80c703e11f1d2819c20b4e0a99c
Rani Malach <rani2011367@gmail.com>
2025-12-15 09:08:34 +0200

    Enable file tree on startup

--- init.lua
+++ init.lua
@@ -51,6 +51,13 @@ require("lazy").setup({
           },
         },
       })
+
+      -- Open file tree on startup
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          require("nvim-tree.api").tree.open()
+        end
+      })
     end,
   },
 
